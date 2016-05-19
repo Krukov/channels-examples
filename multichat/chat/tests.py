ca5f672c1332e62825ae91947e2aa336f6614299
Krukov Dima <glebov.ru@gmail.com>
2016-05-19 18:05:14 +0000

    Update example

--- multichat/chat/tests.py
+++ multichat/chat/tests.py
@@ -1,6 +1,7 @@
 import json
 from django.contrib import auth
-from channels.tests import ChannelTestCase, Client, apply_routes
+from channels.tests import ChannelTestCase, apply_routes
+from channels.tests.http import HttpClient
 
 from .routing import custom_routing, websocket_routing
 from .models import Room
@@ -18,12 +19,13 @@ class TestChat(ChannelTestCase):
         user2 = User.objects.create_user('user_2', 'user_2@test.test', '123')
 
         # login as first user
-        self.client.login(username=user1.username, password='123')
-        self.client.send_and_consume('websocket.connect')
-        self.client.send_and_consume('chat.receive', {'command': 'join', 'room': room_1.id})
+        client = HttpClient()
+        client.login(username=user1.username, password='123')
+        client.send_and_consume('websocket.connect')
+        client.send_and_consume('chat.receive', {'command': 'join', 'room': room_1.id})
 
         # create second client and join room 1 and 2 as user2
-        second_client = Client()
+        second_client = HttpClient()
         second_client.login(username=user2.username, password='123')
         second_client.send_and_consume('websocket.connect')
         second_client.send_and_consume('chat.receive', {'command': 'join', 'room': room_1.id})
@@ -32,18 +34,19 @@ class TestChat(ChannelTestCase):
         second_client.receive()  # drop info message
 
         # first client send message to room 1
-        self.client.send_and_consume('chat.receive',
-                                     {'command': 'send', 'message': 'test message from user 1', 'room': room_1.id})
+        client.send_and_consume('chat.receive',
+                                {'command': 'send', 'message': 'test message from user 1', 'room': room_1.id})
 
         # check that second user apply message
-        self.assertDictEqual(second_client.receive(),
-                             {'text': json.dumps({"message": "test message from user 1",
-                                                  "room": str(room_1.id),
-                                                  "username": user1.username})})
+        received = second_client.receive()
+        self.assertTrue('text' in received)
+        self.assertDictEqual(json.loads(received['text']),
+                             {"message": "test message from user 1",
+                              "room": str(room_1.id),
+                              "username": user1.username})
 
         # first client send message to room 2
-        self.client.send_and_consume('chat.receive', {'command': 'send',
-                                                      'message': 'test message from user 1 for 2 room',
-                                                      'room': room_2.id})
+        client.send_and_consume('chat.receive', {'command': 'send', 'message': 'test message from user 1 for 2 room',
+                                                 'room': room_2.id})
 
         self.assertIsNone(second_client.receive())
